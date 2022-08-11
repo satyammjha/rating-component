53937860a1069fa40e1dad1803214fb5ce832779
Satyam Jha <108946112+satyammjha@users.noreply.github.com>
2022-08-11 13:21:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,30 +14,36 @@ ratingbutton.forEach((ratingvalue) =>
 );
 
 function ratingdisplay(event) {
-  ratingbutton.forEach(() => {
-    event.target.style.backgroundColor = "hsl(25, 97%, 53%)";
-    event.target.style.color = "white";
-    selectedrating = event.target.value;
+  ratingbutton.forEach((el) => {
+    el.classList.remove("selected");
+    el.classList.add("defaultratingbg");
   });
+  event.target.classList.remove("defaultratingbg");
+  event.target.classList.add("selected");
+  selectedrating = event.target.value;
 }
 
 // ShowsContentAfterSubmitting
 submitbtn.addEventListener("click", thankyoumsg);
 function thankyoumsg() {
-  thankyou.src = "./images/illustration-thank-you.svg";
-  thankyou.style.display = "block";
-  starcontainer.style.display = "none";
-  heading.style.marginLeft = "5.5rem";
-  heading.innerHTML = "Thank you!";
-  let rating = selectedrating;
-  ratingtext.innerHTML = `you choose ${rating} out of 5`;
-  ratingtext.style.marginTop = "15px";
-  ratingtext.style.display = "block";
-  thankyoupara.innerHTML =
-    "<p>We appreciate you taking the time to give a rating.</p><p>If you ever need more support,dont hesitate to</p><p>get in touch!</p>";
-  thankyoupara.style.textAlign = "center";
-  thankyoupara.style.marginTop = "30px";
-  thankyoupara.style.marginLeft = "-30px";
-  ratings.style.display = "none";
-  submitbtn.style.display = "none";
+  if (typeof selectedrating === "undefined") {
+    alert("Please Select a Rating Before Submission");
+  } else {
+    thankyou.src = "./images/illustration-thank-you.svg";
+    thankyou.style.display = "block";
+    starcontainer.style.display = "none";
+    heading.style.marginLeft = "5.5rem";
+    heading.innerHTML = "Thank you!";
+    let rating = selectedrating;
+    ratingtext.innerHTML = `you choose ${rating} out of 5`;
+    ratingtext.style.marginTop = "15px";
+    ratingtext.style.display = "block";
+    thankyoupara.innerHTML =
+      "<p>We appreciate you taking the time to give a rating.</p><p>If you ever need more support,dont hesitate to</p><p>get in touch!</p>";
+    thankyoupara.style.textAlign = "center";
+    thankyoupara.style.marginTop = "30px";
+    thankyoupara.style.marginLeft = "-30px";
+    ratings.style.display = "none";
+    submitbtn.style.display = "none";
+  }
 }
